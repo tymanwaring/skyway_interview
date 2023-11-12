cea33590ec5318c7f52bf258d9c82b2ceb92e917
tymanwaring <73245613+tymanwaring@users.noreply.github.com>
2023-11-12 13:38:21 -0700

    Delete src directory

--- src/pages/admin/AdminLogin.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../../components/LoginForm"
-
-export default function AdminLogin() {
-  return (
-    <div>
-    <LoginForm title = "Admin Portal" link = "/admin_dashboard"></LoginForm>
-  </div>
-  );
-}
